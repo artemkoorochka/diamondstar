8c34a94f9b29f9a7eb6e68a70429c456fe32f0f1
artemkoorochka <artem@koorochka.com>
2020-05-21 13:33:08 +0300

    add to header from client

--- koorochka/auth/vk/index.php
+++ koorochka/auth/vk/index.php
@@ -6,21 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://vk.com/js/api/openapi.js?136" type="text/javascript"></script>
 </head>
 <body>
-<script type="text/javascript">
-    VK.init({
-        apiId: 5643587
-    });
-</script>
-<div id="vk_auth"></div>
-<script type="text/javascript">
-    VK.Widgets.Auth('vk_auth', {
-        onAuth: function () {
-            alert("onAuth");
-        }
-    });
+
+Test
+<script>
+
+    setTimeout(function () {
+        alert("Add script to header");
+
+        var script = document.createElement('script');
+        script.src = "https://vk.com/js/api/openapi.js";
+        document.getElementsByTagName('head')[0].appendChild(script);
+
+        script = document.createElement('script');
+        script.src = "https://diamondstar.plus/local/templates/diamond.lite/js/jquery-latest.min.js";
+        document.getElementsByTagName('head')[0].appendChild(script);
+
+
+
+
+    }, 6000);
+
 </script>
 </body>
 </html>
