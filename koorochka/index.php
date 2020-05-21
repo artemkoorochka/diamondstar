30ecef7291f05196a54a190e39d02c1ee9d81937
artemkoorochka <artem@koorochka.com>
2020-05-21 14:37:18 +0300

    Video from server

--- koorochka/index.php
+++ koorochka/index.php
@@ -5,7 +5,24 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("");
 ?>
-  Bitrix page sample
+
+    <video preload="auto"
+           data-setup="{}"
+           webkit-playsinline=""
+           onended="pageVideoPlay(this)"
+           onclick="pageVideoPlay(this)"
+           autoplay=""
+           poster="/images/video/poster.gif"
+           src="/images/video/home.mp4"
+           style="height: 868px; width: auto;">
+
+    </video>
+
+<script>
+    function pageVideoPlay(t) {
+        t.play();
+    }
+</script>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
