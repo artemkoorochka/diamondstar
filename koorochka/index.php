85b4c95b986d499e6149be3fa4ea51216b720601
artemkoorochka <artem@koorochka.com>
2020-05-21 13:35:30 +0300

    Bitrix page

---
+++ koorochka/index.php
@@ -0,0 +1,11 @@
+<?
+/**
+ * @var CMain $APPLICATION
+ */
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("");
+?>
+  Bitrix page sample
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
